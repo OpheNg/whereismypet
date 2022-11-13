45c004bcc2afbe95332fa5b8e4ecfd53639d7ee6
OpheNg <ophe.ng@live.fr>
2022-11-13 17:00:58 +0100

    user profile OK ac photo cl

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
   root to: 'pages#home'
 
   get 'home', to: "pages#home"
   get 'my_page',to:"pages#my_page"
   get "pets", to: "pets#index"
-  get 'profile', to: "pages#profile"
+  get 'profile', to: "users#show"
   get 'my_posts', to: "pages#my_posts"
-  
+
 
   resources :pets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
---
+++ db/migrate/20221112222922_add_info_to_users.rb
@@ -0,0 +1,8 @@
+class AddInfoToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+    add_column :users, :address, :string
+    add_column :users, :tel, :string
+  end
+end
