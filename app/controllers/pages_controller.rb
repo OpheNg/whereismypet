9a618dccddd65a5cbbb8460a9ae8ab32a14e8ad1
OpheNg <ophe.ng@live.fr>
2022-11-17 21:03:28 +0100

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,16 +3,19 @@ class PagesController < ApplicationController
 
   def home
   end
-  def my_page
-    @user= current_user
 
+  def my_page
+    @user = current_user
   end
+
   def profile
     @user = current_user
   end
+
   def my_posts
     @pets = Pet.where(user: current_user)
-    # @pet = Pet.find(params[:pet_id])
     @user = current_user
   end
+
+  
 end
