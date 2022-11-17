5ff337fa2532a369e0028dde33a76945a45faea0
OpheNg <ophe.ng@live.fr>
2022-11-17 22:53:32 +0100

    enlevé attachment

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,6 +25,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content, :attachment)
+    params.require(:message).permit(:content)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
 
-  has_one_attached :attachment,  dependent: :destroy
+  
 
   validates :content, length: { maximum: 100 }
 end
